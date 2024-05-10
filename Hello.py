6e15b15a80d9c41675d74f7e806d25f701d9380f
Niklas Dahmen <niklas.dahmen@iu-study.org>
2024-05-10 09:29:54 +0200

    feat: Changing from Markdown to Infoo

--- Hello.py
+++ Hello.py
@@ -109,8 +109,8 @@ def homepage():
 
 def experiment():
     query = query_db("SELECT * FROM tasks WHERE uuid = %s", st.session_state['task_id'])
-    st.title("Student Hackathon Experiment 2024")
-    st.markdown("<p style=\"border: .5rem solid black;padding: 1rem;background: blue;text: white;\">Imagine you are a consultant hired by a " + query[0]["company"] + " company in " + query[0]["location"] + " facing various challenges in the current business environment. The company produces " + query[0]["product"] + " and is experiencing increased competition, changing market dynamics, and disruptions in the supply chain.<br></p>", unsafe_allow_html=True)
+    st.title("Student Hackathon")
+    st.info("Imagine you are a consultant hired by a " + query[0]["company"] + " company in " + query[0]["location"] + " facing various challenges in the current business environment. The company produces " + query[0]["product"] + " and is experiencing increased competition, changing market dynamics, and disruptions in the supply chain.")
     if st.session_state['round'] == 2:
         #TODO: Add a prompt for the second round
         st.info("This is the second round of the experiment. We have prepared a prompt for you: \n ", icon="ℹ️")
