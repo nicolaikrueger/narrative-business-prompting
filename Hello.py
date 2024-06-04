03d1a6377b4794010664ed90166144ebd9b436c7
Prof. Dr. Nicolai Krüger <c64amg@gmail.com>
2024-06-04 09:21:33 +0000

    assessment skale

--- Hello.py
+++ Hello.py
@@ -211,10 +211,10 @@ def assess_your_story():
     st.write('0 = very low rank; 5 = very high rank')
     selfassessment = {}
 
-    selfassessment["accuracy"] = st.select_slider('Accuracy', options=['very generic', 'somewhat generic', 'undecided', 'somewhat inspiring', 'very inspiring'])
+    selfassessment["accuracy"] = st.select_slider('Accuracy', options=['very general', 'somewhat general', 'undecided', 'somewhat general', 'very general'])
 
 
-    selfassessment["probability"] = st.select_slider('Probability', options=['very generic', 'somewhat generic', 'undecided', 'somewhat inspiring', 'very inspiring'])
+    selfassessment["probability"] = st.select_slider('Probability', options=['very realistic', 'somewhat realistic', 'undecided', 'somewhat far-fetched', 'very far-fetched'])
 
     selfassessment["creativity"] = st.select_slider('Creativity', options=['very generic', 'somewhat generic', 'undecided', 'somewhat inspiring', 'very inspiring'])
 
